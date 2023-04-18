6fdc948e55a51ba63940e556ba36a19aff62157f
unknown <denusklo@gmail.com>
2023-04-19 01:29:34 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 function App() {
+
+  const [data, setData] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  async function fetchData(): Promise<string> {
+    const response = await fetch('https://example.com/data');
+    const data = await response.json();
+    return data;
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{data.title}</div>
+      <div className="container">
+        <ProjectsPage />
+        <div></div>
+      </div>
+    </>
   );
 }
 
